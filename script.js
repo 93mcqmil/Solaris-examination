c18c9612f05650bc732e08e97edf1575150ec51f
McQueen <mcqueen_1993@hotmail.com>
2023-10-31 14:28:13 +0100

    uppdateringar

---
+++ script.js
@@ -0,0 +1,19 @@
+const url = "https://majazocom.github.io/Data/solaris.json";
+const planetList = document.getElementById("planet-list");
+
+async function getPlanetsData() {
+  //Skapat en funktion för att få all data i json objektet
+  try {
+    const data = await fetch(url);
+    if (data.ok) {
+      const result = await data.json();
+      console.log(result);
+    } else {
+      console.error("Failed to fetch data");
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+getPlanetsData();
