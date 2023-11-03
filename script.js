3c32643dcbaefda32e7450aa75837e0f4884fb16
McQueen <mcqueen_1993@hotmail.com>
2023-11-03 15:01:14 +0100

    nästan färdig

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const url = "https://majazocom.github.io/Data/solaris.json";
 const allPlanets = document.querySelectorAll(".allPlanets");
+const overlay = document.getElementById("overlay");
 let dataInfo; //Var tvunget att göra data global så att den är tillgänglig på andra delar i koden
 const getPlanetsData = async () => {
   //Skapat en funktion för att få fram all data i json objektet
@@ -26,11 +27,42 @@ allPlanets.forEach(function (planet) {
 
     const items = dataInfo.find((item) => item.name === clickedElementId);
     if (items) {
-      document.getElementById("overlay").style.display = "block";
+      overlay.style.display = "block";
+      overlay.style.display = "flex";
 
       console.log(items);
     }
+    showOverlay(items);
+    createStars();
   });
 });
 
 getPlanetsData();
+
+function showOverlay(planet) {
+  document.querySelector(".planetName").textContent = `${planet.name}`;
+  document.querySelector(
+    ".planetLatinName"
+  ).textContent = `${planet.latinName}`;
+  document.querySelector(".planetDescription").textContent = `${planet.desc}`;
+  document.querySelector(
+    ".cirkumFerence"
+  ).textContent = `${planet.circumference}`;
+  document.querySelector(".distance").textContent = `${planet.distance}`;
+  document.querySelector(".maxTemperatur").textContent = `${planet.temp.day}`;
+  document.querySelector(".minTemperatur").textContent = `${planet.temp.night}`;
+  document.querySelector(".månar").textContent = `${planet.moons.join(", ")}`;
+}
+function returnOverlay() {
+  overlay.style.display = "none";
+}
+const starContainer = document.getElementById("stars");
+function createStars() {
+  for (let i = 0; i < 99; i++) {
+    const star = document.createElement("div");
+    star.className = "star";
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.top = `${Math.random() * 100}%`;
+    starContainer.appendChild(star);
+  }
+}
